bca148787eabc0d6cec3d0e1374b47fa74b60eea
a21sahfa <a21sahfa@student.his.se>
2023-04-18 11:40:15 +0200

    la en knapp i första activitet som startar button activity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView((R.layout.activity_main));
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("tag", "hej hej");
+
+                Intent intent = new Intent(MainActivity.this, ButtonActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
